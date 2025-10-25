ba03c28dd570a73464bef4aff8ad2089573621a3
Simon <ogbanjesi@gmail.com>
2025-10-25 15:33:42 +0100

    feat: Implement blog management features including CRUD operations, pagination, and view templates; add Sequelize slugify for SEO-friendly URLs

---
+++ src/modules/blog/migrations/20251025134552-create-blog.cjs
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('blogs', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      is_published: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      slug: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('blogs');
+  }
+};
---
+++ src/modules/blog/models/blog.cjs
@@ -0,0 +1,49 @@
+'use strict';
+const { Model } = require('sequelize');
+const SequelizeSlugify = require('sequelize-slugify');
+
+module.exports = (sequelize, DataTypes) => {
+  class Blog extends Model {
+    static associate(models) {
+      // define association here if needed
+    }
+  }
+
+  Blog.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    image_url: {
+      type: DataTypes.STRING
+    },
+    is_published: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    slug: {
+      type: DataTypes.STRING,
+      unique: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Blog',
+    tableName: 'blogs',
+  });
+
+  // Automatically generate slug from title
+  SequelizeSlugify.slugifyModel(Blog, {
+    source: ['title']
+  });
+
+  return Blog;
+};
